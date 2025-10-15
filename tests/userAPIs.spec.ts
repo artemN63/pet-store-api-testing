a2f12353eb1ee80c4093396a4c5688f8d79827ae
artemN63 <nikishynartem@gmail.com>
2025-10-14 20:45:37 -0500

    its broke never push on prod!!!!!

--- tests/userAPIs.spec.ts
+++ tests/userAPIs.spec.ts
@@ -8,46 +8,6 @@ test.describe('User API Tests', () => {
     const userName = process.env.USERNAME;
     const password = process.env.PASSWORD;
 
-    const createUserRequestBody = {
-        id: "345238",
-        username: "TestUserNameArt123",
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        email: faker.internet.email(),
-        password: "Test1234!",
-        phone: faker.phone.number(),
-        userStatus: 0
-    }
-
-  test('Create a new user', async ({request}) => {
-    const createUserResponse = await request.post(`${BASE_URL}/user`, {
-        data: createUserRequestBody
-    });
-
-    await expect(createUserResponse.status()).toBe(200);
-
-    const createUserResponseBody = await createUserResponse.json();
-    
-    const expectedCreateUserResponseSchema = z.object ({
-        code: z.literal(200),
-        type: z.literal("unknown"),
-        message: z.literal(createUserRequestBody.id.toString())
-    });
-
-    expectedCreateUserResponseSchema.parse(createUserResponseBody);
-});
-    
-test('Get user by UserName', async ({request}) => {
-    let getUserResponse;
-    for (let i = 0; i < 5; i++) {
-        getUserResponse = await request.get(`${BASE_URL}/user/${userName}`);
-        if (getUserResponse.status() === 200) {
-            break;
-        }
-    }
-
-    const getUserResponseBody = await getUserResponse!.json();
-
     const expectedGetUserResponseSchema = z.object ({
         id: z.number(),
         username: z.string(),
@@ -59,6 +19,66 @@ test('Get user by UserName', async ({request}) => {
         userStatus: z.number()
     });
 
+    const createUserRequestBody = {
+        id: "345238",
+        username: userName,
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: password,
+        phone: faker.phone.number(),
+        userStatus: 0
+    }
+
+    const expectedCreateUserResponseSchema = z.object ({
+        code: z.literal(200),
+        type: z.literal("unknown"),
+        message: z.literal(createUserRequestBody.id.toString())
+    });
+
+    const expectedDeleteUserResponseSchema = z.object ({
+        code: z.literal(200),
+        type: z.literal("unknown"),
+        message: z.literal(userName)
+    });
+
+  test('Create a new user', async ({request}) => {
+    const createUserResponse = await request.post(`${BASE_URL}/user`, {
+        data: createUserRequestBody
+    });
+
+    await expect(createUserResponse.status()).toBe(200);
+
+    const createUserResponseBody = await createUserResponse.json();
+
+    expectedCreateUserResponseSchema.parse(createUserResponseBody);
+});
+    
+test('Get user by UserName', async ({request}) => {
+    let getUserResponse;
+    for (let i = 0; i < 15; i++) {
+        getUserResponse = await request.get(`${BASE_URL}/user/${userName}`);
+        if (getUserResponse.status() === 200) {
+            break;
+        }
+    }
+
+    const getUserResponseBody = await getUserResponse!.json();
+
     expectedGetUserResponseSchema.parse(getUserResponseBody);
+})
+
+test('Delete user by UserName', async ({request}) => {
+    let deleteUserResponse;
+    for (let i = 0; i < 15; i++) {
+        deleteUserResponse = await request.delete(`${BASE_URL}/user/${userName}`);
+        if (deleteUserResponse.status() === 200) {
+            break;
+        }
+    }
+
+    const deleteUserResponseBody = await deleteUserResponse!.json();
+
+    expectedDeleteUserResponseSchema.parse(deleteUserResponseBody);
 })
 });

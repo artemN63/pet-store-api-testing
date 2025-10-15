d7a8230158e139bb86ae782cdd6738b38216b2f9
artemN63 <nikishynartem@gmail.com>
2025-10-14 19:46:28 -0500

    created get user by username api test

--- tests/userAPIs.spec.ts
+++ tests/userAPIs.spec.ts
@@ -1,10 +1,13 @@
 import { faker } from '@faker-js/faker';
 import {test, expect} from '@playwright/test';
+import { create } from 'domain';
 import { z } from 'zod';
 
 test.describe('User API Tests', () => {
     const BASE_URL = `${process.env.BASE_URL}${process.env.API_VERSION}`;
-  test('Create a new user', async ({request}) => {
+    const userName = process.env.USERNAME;
+    const password = process.env.PASSWORD;
+
     const createUserRequestBody = {
         id: "345238",
         username: "TestUserNameArt123",
@@ -16,6 +19,7 @@ test.describe('User API Tests', () => {
         userStatus: 0
     }
 
+  test('Create a new user', async ({request}) => {
     const createUserResponse = await request.post(`${BASE_URL}/user`, {
         data: createUserRequestBody
     });
@@ -24,12 +28,33 @@ test.describe('User API Tests', () => {
 
     const createUserResponseBody = await createUserResponse.json();
     
-    const expectedResponseSchema = z.object ({
+    const expectedCreateUserResponseSchema = z.object ({
         code: z.literal(200),
         type: z.literal("unknown"),
         message: z.literal(createUserRequestBody.id.toString())
     });
 
-    expectedResponseSchema.parse(createUserResponseBody);
+    expectedCreateUserResponseSchema.parse(createUserResponseBody);
 });
+    
+test('Get user by UserName', async ({request}) => {
+    const getUserResponse = await request.get(`${BASE_URL}/user/${userName}`);
+
+    await expect(getUserResponse.status()).toBe(200);
+
+    const getUserResponseBody = await getUserResponse.json();
+
+    const expectedGetUserResponseSchema = z.object ({
+        id: z.literal(createUserRequestBody.id),
+        username: z.literal(createUserRequestBody.username),
+        firstName: z.literal(createUserRequestBody.firstName),
+        lastName: z.literal(createUserRequestBody.lastName),
+        email: z.literal(createUserRequestBody.email),
+        password: z.literal(createUserRequestBody.password),
+        phone: z.literal(createUserRequestBody.phone),
+        userStatus: z.literal(createUserRequestBody.userStatus)
+    });
+
+    expectedGetUserResponseSchema.parse(getUserResponseBody);
+})
 })

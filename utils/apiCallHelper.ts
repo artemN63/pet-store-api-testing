c6d0455eca0c57025392a5b4660a6997aa5dd957
artemN63 <nikishynartem@gmail.com>
2025-10-16 19:47:09 -0500

    easy method

--- utils/apiCallHelper.ts
+++ utils/apiCallHelper.ts
@@ -1,10 +1,10 @@
 import {APIRequestContext, APIResponse, Response} from '@playwright/test';
 import { ZodTypeAny } from 'zod';
 
-export async function getAPI(request: APIRequestContext, url: string, expectedStatusCode: number, expectedSchema: ZodTypeAny): Promise<APIResponse> {
+export async function getAPI(request: APIRequestContext, url: string, expectedStatusCode: number, expectedSchema: ZodTypeAny, parameters: Record<string, any>): Promise<APIResponse> {
     let getUserResponse;
     for (let i = 0; i < 15; i++) {
-        getUserResponse = await request.get(url);
+        getUserResponse = await request.get(url, { params: parameters });
         if (getUserResponse.status() === expectedStatusCode) {
             const getUserResponseBody = await getUserResponse!.json();
             expectedSchema.parse(getUserResponseBody);
@@ -49,26 +49,4 @@ export async function deleteAPI(request: APIRequestContext, url: string, expecte
     }
 
     throw new Error(`DELETE request to ${url} did not return status code ${expectedStatusCode} within 15 attempts.`);
-}
-
-export async function logIn(request: APIRequestContext, url: string, username: string, password: string, expectedStatusCode: number, expectedSchema: ZodTypeAny): Promise<APIResponse> {
-    const logIn = await request.get(url, { data: { username, password } });
-    if (logIn.status() === expectedStatusCode) {
-        const logInResponseBody = await logIn.json();
-        expectedSchema.parse(logInResponseBody);
-        return logIn;
-    }
-
-    throw new Error(`GET request to ${url} did not return status code ${expectedStatusCode}.`);
-}
-
-export async function logOut(request: APIRequestContext, url: string, expectedStatusCode: number, expectedSchema: ZodTypeAny): Promise<APIResponse> {
-    const logOut = await request.get(url);
-    if (logOut.status() === expectedStatusCode) {
-        const logOutResponseBody = await logOut.json();
-        expectedSchema.parse(logOutResponseBody);
-        return logOut;
-    }
-
-    throw new Error(`GET request to ${url} did not return status code ${expectedStatusCode}.`);
 }

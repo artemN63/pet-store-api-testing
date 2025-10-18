949fcb78d38c537ef9a3d2a7aa18751bee77aa9f
artemN63 <nikishynartem@gmail.com>
2025-10-17 20:42:44 -0500

    made it flexiable

--- tests/storeAPIs.spec.ts
+++ tests/storeAPIs.spec.ts
@@ -1,16 +1,16 @@
 import { test, expect } from '@playwright/test';
+import { z } from 'zod';
+import {getAPI} from '../utils/apiCallHelper';
 
 test.describe('Pet API Tests', () => {
     const BASE_URL = `${process.env.BASE_URL}${process.env.API_VERSION}`;
 
+    const expectedInventoryResponseSchema = z
+        .record(z.string(), z.number())
+        .refine((data) => Object.keys(data).length === 14
+    );
+
     test('Get Inventory', async ({ request }) => {
-        const response = await request.get(`${BASE_URL}/store/inventory`);
-        
-        expect(response.status()).toBe(200);
-        
-        const inventoryData = await response.json();
-        
-        const entries = Object.entries(inventoryData);
-        expect(entries.length).toBe(14);
+        await getAPI(request, `${BASE_URL}/store/inventory`, 200, expectedInventoryResponseSchema);
     });
 });
